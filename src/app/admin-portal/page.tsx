0d589d409a038a62d2acc1f45ad8d8df50e1a750
Syahrul Setiawan <syahrulsetiaw626@gmail.com>
2025-11-05 20:37:27 +0700

    dropdown draft

--- src/app/admin-portal/page.tsx
+++ src/app/admin-portal/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import {AsyncSelect} from '@/components/custom/AsyncSelect';
 import MyFullLoadingPage from '@/components/MyFullLoadingPage';
 import {Button} from '@/components/ui/button';
 import {useLoading} from '@/hooks/useLoading';
@@ -23,7 +24,28 @@ export default function Page() {
       <MyFullLoadingPage isLoading={isLoading} />
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="bg-muted/50 aspect-video rounded-xl p-4">
-          <Button size={'sm'}>Primary</Button>
+          <div className="grid gap-2">
+            <div>
+              <Button size={'sm'}>Primary</Button>
+            </div>
+
+            <AsyncSelect
+              label="Static"
+              options={[
+                {label: 'Active', value: 'active'},
+                {label: 'Inactive', value: 'inactive'}
+              ]}
+              onChange={(v) => console.log(v)}
+            />
+
+            <AsyncSelect
+              label="With data source"
+              fetchUrl="/customers"
+              placeholder="Select Customer..."
+              params={{projectid: 2}}
+              onChange={(v) => console.log(v)}
+            />
+          </div>
         </div>
         <div className="bg-muted/50 aspect-video rounded-xl" />
         <div className="bg-muted/50 aspect-video rounded-xl" />
