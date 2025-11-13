b36d84d68f5700bc3cbae01e2e8263fd8e1f9723
Syahrul Setiawan <syahrulsetiaw626@gmail.com>
2025-11-13 22:43:25 +0700

    update

--- src/types/CompanySettingTypes.ts
+++ src/types/CompanySettingTypes.ts
@@ -12,4 +12,13 @@ export interface ICompanySetting {
   company_province: string;
   company_city: string;
   company_postal_code: string;
+
+  config_date_format: string;
+  config_currency_format: string /** #,### | #.### */;
+  config_timezone: string /** WIB | WITA | WIT */;
+  config_currency_code: string;
+  config_default_language: string /** id | en */;
+  config_accounting_fiscal_year_start: string /** YYYY-MM */;
+  config_available_vat: boolean;
+  config_vat_percentage: number;
 }
